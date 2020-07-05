1dc286965a6d19364ffab4d078003bb297cbf382
Mr.T0B1N9 <66794495+MrT0B1N9@users.noreply.github.com>
2020-07-05 21:22:38 +0700

    Create pin.php

---
+++ pin.php
@@ -0,0 +1,23 @@
+<?php
+date_default_timezone_set('Asia/Jakarta');
+include "functionx.php";
+echo color("white","Token: ");
+$token = trim(fgets(STDIN));
+ setpin:
+         echo "\n".color("white","SETPIN..!!!: y/n ");
+         $pilih1 = trim(fgets(STDIN));
+         if($pilih1 == "y" || $pilih1 == "Y"){
+         //if($pilih1 == "y" && strpos($no, "628")){
+         echo color("white","▬▬▬▬▬▬▬▬▬▬▬▬▬▬ PIN MU = 147258 ▬▬▬▬▬▬▬▬▬▬▬▬")."\n";
+         $data2 = '{"pin":"147258"}';
+         $getotpsetpin = request("/wallet/pin", $token, $data2, null, null, $uuid);
+         echo "Otp pin: ";
+         $otpsetpin = trim(fgets(STDIN));
+         $verifotpsetpin = request("/wallet/pin", $token, $data2, null, $otpsetpin, $uuid);
+         echo $verifotpsetpin;
+         }else if($pilih1 == "n" || $pilih1 == "N"){
+         die();
+         }else{
+         echo color("white","-] GAGAL!!!\n");
+         }
+         }
