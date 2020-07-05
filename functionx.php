c141bce73bbd23f0bb6f52cb4b7d6150633e6f5f
Mr.T0B1N9 <66794495+MrT0B1N9@users.noreply.github.com>
2020-07-05 20:44:49 +0700

    Update functionx.php

--- functionx.php
+++ functionx.php
@@ -10,7 +10,7 @@ $header[] = "X-AppVersion: 3.50.1";
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
 $header[] = "X-User-Locale: id_ID";
-$header[] = "X-Location: -8.1735593,113.7014891";
+$header[] = "X-Location: -8.670897696801344,115.21709751337767";
 $header[] = "X-Location-Accuracy: 3.0";
 if ($pin):
 $header[] = "pin: $pin";
